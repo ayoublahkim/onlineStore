5cb5731efe710a85fbf8f4491de93d7eb8e455ed
alahkim <alahkim@FR003882.corp.capgemini.com>
2018-01-26 15:54:20 +0100

    First commit : setup complited

---
+++ onlinestorebackend/src/main/java/org/sid/onlinestorebackend/App.java
@@ -0,0 +1,13 @@
+package org.sid.onlinestorebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
