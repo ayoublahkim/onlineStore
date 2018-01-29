7ca299e4ed7cb791637d71ff5d8c62dd8e9dc206
alahkim <alahkim@FR003882.corp.capgemini.com>
2018-01-29 15:28:42 +0100

    Successfully load static resources

--- onlinestore/src/main/java/org/sid/onlinestore/controller/PageController.java
+++ onlinestore/src/main/java/org/sid/onlinestore/controller/PageController.java
@@ -1,7 +1,9 @@
 package org.sid.onlinestore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,5 +15,25 @@ public class PageController {
 		mv.addObject("greeting","welcome to spring mvc");
 		return mv;
 	}
+	@RequestMapping(value= {"/test"}) 
+	public ModelAndView test(@RequestParam("greeting") String greeting)
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/test/{greeting}"}) 
+	public ModelAndView test2(@PathVariable("greeting") String ggreeting)
+	{
+		if(ggreeting==null)
+		{
+			ggreeting="hello there";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",ggreeting);
+		return mv;
+	}
 
 }
+
