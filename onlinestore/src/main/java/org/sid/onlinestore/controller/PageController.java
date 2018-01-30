1ff507b9113b43dfef244827e3094df73b0c0b0c
alahkim <alahkim@FR003882.corp.capgemini.com>
2018-01-30 11:23:01 +0100

    finalisation front

--- onlinestore/src/main/java/org/sid/onlinestore/controller/PageController.java
+++ onlinestore/src/main/java/org/sid/onlinestore/controller/PageController.java
@@ -8,11 +8,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
+	
+	
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index()
 	{
 		ModelAndView mv=new ModelAndView("page");
 		mv.addObject("greeting","welcome to spring mvc");
+		mv.addObject("userClickHome",true);
+		mv.addObject("title","Home");
+		return mv;
+	}
+	@RequestMapping(value= "/about")
+	public ModelAndView about()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("userClickAbout",true);
+		mv.addObject("title","About Us");
+		return mv;
+	}
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("userClickContact",true);
+		mv.addObject("title","Contact Us");
 		return mv;
 	}
 	@RequestMapping(value= {"/test"}) 
---
+++ onlinestore/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,8 @@
+<footer>
+
+      <div class="container footer center">
+      <hr/>
+        <p class="m-0 text-center text-black">Copyright &copy; Ayoub LAHKIM 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
